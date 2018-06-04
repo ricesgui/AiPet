2abbc9aff115338b2d7b721d5fd924bc0a0ac93c
Jun <CSerwangjun@163.com>
2018-06-04 23:13:30 +0800

    王俊:添加注册逻辑

--- src/main/java/cn/edu/scnu/it/aipet/controller/registerController.java
+++ src/main/java/cn/edu/scnu/it/aipet/controller/registerController.java
@@ -1,6 +1,8 @@
 package cn.edu.scnu.it.aipet.controller;
 
 import cn.edu.scnu.it.aipet.pojo.User;
+import cn.edu.scnu.it.aipet.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,6 +10,10 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller("registerController")
 @RequestMapping("/test")
 public class registerController {
+
+    @Autowired
+    private UserService userService=null;
+
     @RequestMapping("register")
     public ModelAndView register(User user){
 
