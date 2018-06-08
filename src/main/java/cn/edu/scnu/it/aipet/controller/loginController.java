08fe99b9cd456d75dce94b93f1e4da9ed88f3297
Jun <CSerwangjun@163.com>
2018-06-08 11:05:54 +0800

    王俊:主页的登录大版块添加处理链接

--- src/main/java/cn/edu/scnu/it/aipet/controller/loginController.java
+++ src/main/java/cn/edu/scnu/it/aipet/controller/loginController.java
@@ -5,6 +5,7 @@ import cn.edu.scnu.it.aipet.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
@@ -15,7 +16,7 @@ public class loginController {
     @Autowired
     private UserService userService = null;
 
-    @RequestMapping("/loginEvent")
+    @RequestMapping(value = "/loginEvent",method =RequestMethod.POST)
     public ModelAndView judge(String email, String password, HttpSession session) {
         ModelAndView mv = new ModelAndView();
         User user = userService.selectUser(email);
