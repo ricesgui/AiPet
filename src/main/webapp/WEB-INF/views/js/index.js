71e7a08ed0000260ba71b5dc89856a98921c63af
ricesgui01 <2524366546@qq.com>
2018-06-13 20:52:56 +0800

    xiaogui

--- src/main/webapp/WEB-INF/views/js/index.js
+++ src/main/webapp/WEB-INF/views/js/index.js
@@ -67,33 +67,8 @@
                     bigImg.src="images/maomi.jpg";   //给img元素的src属性赋值  
                     bigImg.height="400";  //320个像素 不用加px  
                   
-                    im.appendChild(bigImg);      //为dom添加子元素img  
-                    $("#third").click(function logout()
-                {
-            var xmlhttp;
-            if (window.XMLHttpRequest)
-            {
-                // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-                xmlhttp=new XMLHttpRequest();
-            }
-            else
-            {
-                // IE6, IE5 浏览器执行代码
-                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-            }
-            var log = {"evenType":"1"};
-            
-         xmlhttp.onreadystatechange=function()
-            {
-                if (xmlhttp.readyState==4 && xmlhttp.status==200)
-                { 
-                    var myJSON = JSON.stringify(log);   
-                }
-            }
-            xmlhttp.open("POST","/views/log_out",true);
-            xmlhttp.send();
-});
-            }
+                    im.appendChild(bigImg);      //为dom添加子元素img                    
+                    }
         }
         xmlhttp.open("POST","/resources/get_name",true);
         xmlhttp.send();
