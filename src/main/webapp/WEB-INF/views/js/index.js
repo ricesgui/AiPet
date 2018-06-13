13af35cb493b6985ade451e635780767870e79bf
ricesgui01 <2524366546@qq.com>
2018-06-13 20:20:20 +0800

    xiaogui

--- src/main/webapp/WEB-INF/views/js/index.js
+++ src/main/webapp/WEB-INF/views/js/index.js
@@ -4,7 +4,6 @@
 	 });
 	alert("欢迎登录Apet.com!");
 */
-
         /*    $("#third").click(function logout(){
                 var log = {"evenType":"1"};
             $.ajax({ 
@@ -69,6 +68,31 @@
                     bigImg.height="400";  //320个像素 不用加px  
                   
                     im.appendChild(bigImg);      //为dom添加子元素img  
+                    $("#third").click(function logout()
+                {
+            var xmlhttp;
+            if (window.XMLHttpRequest)
+            {
+                // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+                xmlhttp=new XMLHttpRequest();
+            }
+            else
+            {
+                // IE6, IE5 浏览器执行代码
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            var log = {"evenType":"1"};
+            
+         xmlhttp.onreadystatechange=function()
+            {
+                if (xmlhttp.readyState==4 && xmlhttp.status==200)
+                { 
+                    var myJSON = JSON.stringify(log);   
+                }
+            }
+            xmlhttp.open("POST","/views/log_out",true);
+            xmlhttp.send();
+});
             }
         }
         xmlhttp.open("POST","/resources/get_name",true);
