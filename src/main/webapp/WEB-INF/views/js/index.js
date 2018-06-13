4d69d43d2ad40a1c268a33aa556b0feef89c670c
ricesgui01 <2524366546@qq.com>
2018-06-13 21:29:52 +0800

    xiaogui

--- src/main/webapp/WEB-INF/views/js/index.js
+++ src/main/webapp/WEB-INF/views/js/index.js
@@ -4,6 +4,57 @@
 	 });
 	alert("欢迎登录Apet.com!");
 */
+ function showName(str)
+        {
+            var xmlhttp;
+            if (str=="")
+            {
+                document.getElementById("txtHint").innerHTML="";
+                return;
+            }
+            if (window.XMLHttpRequest)
+            {
+                // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+                xmlhttp=new XMLHttpRequest();
+            }
+            else
+            {
+                // IE6, IE5 浏览器执行代码
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange=function()
+            {
+                if (xmlhttp.readyState==4 && xmlhttp.status==200)
+                {
+                    var obj =JSON.parse(xmlhttp.responseText);
+                    document.getElementById("txtHint").innerHTML=obj.user_name;
+                    //实现第二个提示
+                    var node1 = document.createElement("span");
+                    var text1 = document.createTextNode("您已登录");
+                    node1.appendChild(text1); 
+                    var parent1 = document.getElementById("second");
+                    var node2 = document.getElementById("s1");
+                    parent1.replaceChild(node1,node2);
+                    var node3 = document.getElementById("third");
+                    node3.innerHTML="退出";
+                    //var node4 = document.getElementById("four");
+                   // node4.href = "login.html";
+                   $("#yincang").hide();
+                    $("#div-m").hide();
+                    var im = document.getElementById("denglu");
+                    var bigImg = document.createElement("img");     //创建一个img元素  
+                    bigImg.src="images/maomi.jpg";   //给img元素的src属性赋值  
+                    bigImg.height="400";  //320个像素 不用加px  
+                  
+                    im.appendChild(bigImg);      //为dom添加子元素img   
+                }
+            }
+            xmlhttp.open("POST","/resources/get_name",true);
+            xmlhttp.send();
+        }
+        window.onload=showName;//不要括号
+
+
         /*    $("#third").click(function logout(){
                 var log = {"evenType":"1"};
             $.ajax({ 
@@ -24,58 +75,9 @@
             });
 
 			
-       */     
-    function showName(str)
-    {
-        var xmlhttp;
-        if (str=="")
-        {
-            document.getElementById("txtHint").innerHTML="";
-            return;
-        }
-        if (window.XMLHttpRequest)
-        {
-            // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-            xmlhttp=new XMLHttpRequest();
-        }
-        else
-        {
-            // IE6, IE5 浏览器执行代码
-            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange=function()
-        {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-                var obj =JSON.parse(xmlhttp.responseText);
-                document.getElementById("txtHint").innerHTML=obj.user_name;
-                //实现第二个提示
-                var node1 = document.createElement("span");
-                var text1 = document.createTextNode("您已登录");
-                node1.appendChild(text1); 
-                var parent1 = document.getElementById("second");
-                var node2 = document.getElementById("s1");
-                parent1.replaceChild(node1,node2);
-                var node3 = document.getElementById("third");
-                node3.innerHTML="退出";
-                //var node4 = document.getElementById("four");
-               // node4.href = "login.html";
-               $("#yincang").hide();
-                $("#div-m").hide();
-                var im = document.getElementById("denglu");
-                var bigImg = document.createElement("img");     //创建一个img元素  
-                bigImg.src="images/maomi.jpg";   //给img元素的src属性赋值  
-                bigImg.height="400";  //320个像素 不用加px  
-              
-                im.appendChild(bigImg);      //为dom添加子元素img                    
-             }
-        }
-        xmlhttp.open("POST","/resources/get_name",true);
-        xmlhttp.send();
-    }
-    
-      window.onload=showName;//不要括号
-
+         
+ 
+*/
    /*   function logout()
       {
             var xmlhttp;
