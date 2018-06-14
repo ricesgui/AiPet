02f5aa936285435bb7d12f29bf3db607b0c5b862
ricesgui01 <2524366546@qq.com>
2018-06-14 19:35:32 +0800

    xiaogui-login

--- src/main/webapp/WEB-INF/views/js/index.js
+++ src/main/webapp/WEB-INF/views/js/index.js
@@ -26,7 +26,24 @@
                     if(obj.user_name==null)
                     document.getElementById("txtHint").innerHTML="gdga";
                     else  
+                    {
                      document.getElementById("txtHint").innerHTML=obj.user_name;
+                      //实现第二个提示
+                    
+                    document.getElementById("s1").innerHTML="您已登录";                                     
+                    document.getElementById("third").innerHTML="退出";
+        
+                    //var node4 = document.getElementById("four");
+                    //node4.href = "login.html";
+                    $("#yincang").hide();
+                    $("#div-m").hide();
+                    var im = document.getElementById("denglu");
+                    var bigImg = document.createElement("img");     //创建一个img元素  
+                    bigImg.src="images/maomi.jpg";   //给img元素的src属性赋值  
+                    bigImg.height="400";  //320个像素 不用加px  
+                  
+                    im.appendChild(bigImg);      //为dom添加子元素img  
+                    }
                 }
             }
             xmlhttp.open("POST","/resources/get_name",true);
