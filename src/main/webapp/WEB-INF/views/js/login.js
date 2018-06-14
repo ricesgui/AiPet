a6eb3ac84472394edc2dbfddc10c9001119b2d56
ricesgui01 <2524366546@qq.com>
2018-06-14 20:31:25 +0800

    xiaogui-logout

--- src/main/webapp/WEB-INF/views/js/login.js
+++ src/main/webapp/WEB-INF/views/js/login.js
@@ -1,31 +1,4 @@
-
-
-     /* $("#third").click(function logout(){
-                    var xmlhttp1;
-                    var log ={"evenType":"1"};
-                    if (window.XMLHttpRequest)
-                    {
-                        // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-                        xmlhttp1=new XMLHttpRequest();
-                    }
-                    else
-                    {
-                        // IE6, IE5 浏览器执行代码
-                        xmlhttp1=new ActiveXObject("Microsoft.XMLHTTP");
-                    }
-                    xmlhttp.onreadystatechange=function()
-                    {
-                        if (xmlhttp1.readyState==4 && xmlhttp1.status==200)
-                        {
-                            
-                            var log =JSON.stringify(log);
-                            alert("test");
-                        }
-                    }
-                    xmlhttp1.open("POST","/views/log_out",true);
-                    xmlhttp1.send();
-                   }*/
-     /*  $("#third").click(function showName()
+    /*  $("#third").click(function showName()
         {
             var xmlhttp;
             var log = {"event":{"evenType":"1"}};
@@ -60,7 +33,7 @@
         $.ajax({
             type:"POST",
             url:"/views/log_out",
-            content-Type:"application/json",
+            contentType:"application/json",
             dataType:"json",
             data:JSON.stringify({"event":{"evenType":"1"}}),
             success:function(){
