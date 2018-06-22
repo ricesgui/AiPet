65ee1dce4bcf6c5a311f0b852c9bd30d010f234e
Jun <CSerwangjun@163.com>
2018-06-22 10:57:22 +0800

    王俊:文件接口完成，未测

---
+++ src/main/java/cn/edu/scnu/it/aipet/util/placeoutJson.java
@@ -0,0 +1,24 @@
+package cn.edu.scnu.it.aipet.util;
+
+import cn.edu.scnu.it.aipet.pojo.Pet;
+import cn.edu.scnu.it.aipet.pojo.Placeout;
+
+public class PlaceoutJson {
+    private Pet pet=null;
+    private Placeout placeout=null;
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public Placeout getPlaceout() {
+        return placeout;
+    }
+
+    public void setPlaceout(Placeout placeout) {
+        this.placeout = placeout;}
+}
