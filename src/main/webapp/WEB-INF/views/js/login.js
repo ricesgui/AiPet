9aca84e633edf5df87a2a2880fb797aecfffa258
ricesgui01 <2524366546@qq.com>
2018-06-22 09:53:37 +0800

    xiaogui-

--- src/main/webapp/WEB-INF/views/js/login.js
+++ src/main/webapp/WEB-INF/views/js/login.js
@@ -1,35 +1,31 @@
-
-/*$(document).ready(function(){
-     $("#log").click(function(){  
+$(function() { 
+$("#email").blur(function(){   
     var email=$("#email").val();  
-    var password=$("#password").val();
-    if(email==""&&password==""){  alert("请输入您的用户名和密码！"); }
-     else {
-        $.ajax({  
-        type: "POST",   //请求方式  
-        url: "/views/log_in",    //请求的url地址  
+    if(email!=""){  
+     $.ajax({  
+        url: "/check/username",    //请求的url地址  
         contentType: "application/json; charset=utf-8",  
-        data: JSON.stringify({"email":email,"password":password}), //使用这个函数可以转化为json格式   //参数值  
         dataType: "json",   //返回格式为json  
-        //async: true,//请求是否异步，默认为异步，这也是ajax重要特性  
-         success: function (data) {//回调函数
-                if(data.status=='ok'){
-                    window.location.href="index.html";
-                    //设置cookies
-                }
-                else if(data.status=='fail')
-                    {
-                        if (data.errorType=='1') alert("邮箱不存在！");
-                        else if (data.errorType=='2') alert("邮箱或密码错误，请重新输入！");
-                }
-                
+        async: true,//请求是否异步，默认为异步，这也是ajax重要特性  
+        data: JSON.stringify({"email":email}), //使用这个函数可以转化为json格式   //参数值  
+        type: "POST",   //请求方式  
+         /*  因为服务器端返回的是json对象所以可以直接用对象名。属性名 */  
+       success: function (data) {
+                if(data.validity=='fail')
+                {$("#e2").html("邮箱不存在");                
+                $("#e2"）.css({"color":"red","font-size":"20px"});
+              }
+             
+                else{ 
+                    $("#e2").html("邮箱存在");
+                    $("#e2").css({"color":"green","font-size":"20px"});
+            }
             },
         error: function (xhr,error) {
                 console.debug(xhr);
                 console.debug(error);
-            }  
+            }      
      });  
     }
 });
- });
-*/
+ }
