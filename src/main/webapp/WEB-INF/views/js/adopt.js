2a3704973d23d9e94071aa43c0d680fecf5031ce
ricesgui01 <2524366546@qq.com>
2018-06-22 22:34:42 +0800

    xiaogui-fabu

--- src/main/webapp/WEB-INF/views/js/adopt.js
+++ src/main/webapp/WEB-INF/views/js/adopt.js
@@ -1,6 +1,6 @@
   var stu = {};
-//var input = document.getElementById('file');
-    $("file").addEventListener('change', function(e){
+var even= document.getElementById('file');
+    even.addEventListener('change', function(e){
         var file = e.target.files[0];
         var formData = new FormData();
         formData.append('file', file);
@@ -38,29 +38,29 @@
  $("#btn").click(function(){
 //  serializeArray()将form表单控件中的数据序列化成数组,数组中含有若干对象,对象包含对应控件的name和value
  
-  // var stu = {};
-  // var infor = $('#form').serializeArray();
-  // console.log(infor);
-  // for (var i=0;i<infor.length;i++) {
-  // var obj=infor[i];
-  // stu[obj.name] = obj.value;
-  // }
-  // stu["petDescri"]=document.getElementById("txt1").value;
-  // console.log(stu);
+  var stu = {};
+  var infor = $('#form').serializeArray();
+  console.log(infor);
+  for (var i=0;i<infor.length;i++) {
+  var obj=infor[i];
+  stu[obj.name] = obj.value;
+  }
+  stu["petDescri"]=document.getElementById("txt1").value;
+  console.log(stu);
    //发送ajax请求
-  var petspecies=$("input[name='petSpecies']:checked").val();  
-  var name=$("input[name='petName']").val();
-  var sex=$("input[name='petSex']:checked").val(); 
-  var age_year=$("input[name='petAgeYear']").val();
-  var age_month=$("input[name='petAgeMonth']").val();
-  var weight=$("input[name='weight']").val();
-  var petdescri=$("input[name='petDescri']").val();
-  var petphotourl=stu["petPhotourl"];
-  var placeouttitle=$("input[name='placeOutTitle").val();
-  var realname=$("input[name='realName']").val();
-  var sex=$("input[name='userSex']").val();
-  var telNumber=$("input[name='telNumber']").val();
-  var address=$("input[name='userAddress']").val();
+  // var petspecies=$("input[name='petSpecies']:checked").val();  
+  // var name=$("input[name='petName']").val();
+  // var sex=$("input[name='petSex']:checked").val(); 
+  // var age_year=$("input[name='petAgeYear']").val();
+  // var age_month=$("input[name='petAgeMonth']").val();
+  // var weight=$("input[name='weight']").val();
+  // var petdescri=$("input[name='petDescri']").val();
+  // var petphotourl=stu["petPhotourl"];
+  // var placeouttitle=$("input[name='placeOutTitle").val();
+  // var realname=$("input[name='realName']").val();
+  // var sex=$("input[name='userSex']").val();
+  // var telNumber=$("input[name='telNumber']").val();
+  // var address=$("input[name='userAddress']").val();
 
 
   $.ajax({
@@ -83,3 +83,4 @@
             }  
   });
  });
+
