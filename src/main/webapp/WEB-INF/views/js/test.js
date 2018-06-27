47c4b246490eb2dc83ae945e812a8dd83253c5e8
ricesgui01 <2524366546@qq.com>
2018-06-27 19:36:59 +0800

    xiaogui

--- src/main/webapp/WEB-INF/views/js/test.js
+++ src/main/webapp/WEB-INF/views/js/test.js
@@ -1,7 +1,5 @@
 $(function(){
 	//点击个人中心，进到页面，向服务器请求数据回填
-        function infomation()
-        {
    		$.ajax({ 
         type: "POST",   //请求方式  
         url: "/views/get_user_info",    //请求的url地址  
@@ -9,13 +7,13 @@ $(function(){
         dataType: "json",   //返回格式为json   
         success: function (users){//回调函数
         //向服务器请求用户个人信息，并显示
-            $("#address9").html(user.address);  
-            $("#age_year9").html(user.age_year);
-            $("#age_month9").html(user.age_month);
-            $("#datetime9").html(user.datetime);
-            $("#petdescri9").html(user.petdescri);        
-            // var html="<div class=\"caption\"><h4>地址："+users.address+"</h4><p>年龄："+users.age_year+"年"+users.age_month+"月"+"<br>"+"发布时间："+users.datetime+"<br>"+"发布人描述信息："+users.petdescri+"</p></div>";
-            // $("#info").after(html);
+            // $("#address9").html(user.address);  
+            // $("#age_year9").html(user.age_year);
+            // $("#age_month9").html(user.age_month);
+            // $("#datetime9").html(user.datetime);
+            // $("#petdescri9").html(user.petdescri);        
+            var html="<div class=\"col-xs-6 col-xs-3\" id=\"para\"><div class=\"thumbnail\" id=\"detail\"><div class=\"caption\"><h4>地址："+users.address+"</h4><p>年龄："+users.age_year+"年"+users.age_month+"月"+"<br>"+"发布时间："+users.datetime+"<br>"+"发布人描述信息："+users.petdescri+"</p></div>";
+            $("#info").append(html);
         },
         error: function(){
         	console.log("获取.fail");
