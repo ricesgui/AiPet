119d61e38facf839f6b797c820ec1e1e74e1b3d8
ricesgui01 <2524366546@qq.com>
2018-06-27 22:32:17 +0800

    xiai

--- src/main/webapp/WEB-INF/views/js/user.js
+++ src/main/webapp/WEB-INF/views/js/user.js
@@ -1,14 +1,74 @@
-$(function(){
+window.onload=function(){
+    $("#user_info").hide();
+    onload2();
+    onload3();
+    onload4();
+   }
+function onload2(){
+        var xmlhttp;          
+        if (window.XMLHttpRequest)
+        {
+            // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+            xmlhttp=new XMLHttpRequest();
+        }
+        else
+        {
+            // IE6, IE5 浏览器执行代码
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function()
+        {
+            if (xmlhttp.readyState==4 && xmlhttp.status==200)
+            {
+                var obj =JSON.parse(xmlhttp.responseText);
+                if(obj.user_name==null)
+                {
+                     alert("您还没有登录，请先登录");
+                     window.location.href="login.html";                  
+                }                  
+                else  
+                {
+                document.getElementById("txtHint").innerHTML=obj.user_name;
+                  //实现第二个提示                   
+                document.getElementById("s1").innerHTML="您已登录";                                     
+                document.getElementById("third").innerHTML="退出"; 
+                $("#user_info").show();       
+                $("#yincang").hide();                
+                }
+            }
+        }
+        xmlhttp.open("POST","/resources/get_name",true);
+        xmlhttp.send();
+    }
+function onload3(){
+           $("#third").click(function(){
+            $.ajax({
+                type: "POST",
+                url: "/views/log_out",
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify({"eventType":"1"}),
+                dataType: "json",
+                success: function (data) {//回调函数
+                 if(data.status=='success'){
+                    window.location.href="index.html";
+                }
+                 else console.log("error");
+                },
+                error: function (xhr,error) {
+                    console.log("index.fail");
+                }
+            });
+        });
+       }
+       function onload4(){
 	//点击个人中心，进到页面，向服务器请求数据回填
    		$.ajax({  
         type: "POST",   //请求方式  
         url: "/resources/get_user_info",    //请求的url地址  
         contentType: "application/json; charset=utf-8",  
         dataType: "json",   //返回格式为json  
-        //data:stringify({"start":0,"size":1}), 
         success: function (data) {//回调函数
-        //向服务器请求用户个人信息，并显示
-        
+        //向服务器请求用户个人信息，并显示       
         console.log(data);
         console.log(data.user.name);
             $("#name").val(data.user.name);
@@ -24,7 +84,8 @@ $(function(){
         error: function(){
         	console.log("获取.fail");
         }
-    });      
+    });  
+    }    
 //点击修改按钮触发函数，上传服务器修改
 $("#btn-save").click(function(){
     var name =$("#name").val();
@@ -38,7 +99,7 @@ $("#btn-save").click(function(){
     if(name!=""&&email!=""&&password!=""&&number!=""){  
      $.ajax({  
         type: "POST",   //请求方式  
-        url: "/views/",    //请求的url地址  
+        url: "/views/registerEvent",    //请求的url地址  
         contentType: "application/json; charset=utf-8",  
         data: JSON.stringify({"name":name,"email":email,"password":password,"realname":realname,"sex":sex,"number":number,"address":address}), 
         dataType: "json",   //返回格式为json   
@@ -69,5 +130,5 @@ $("#btn-save").click(function(){
       
  });
     
- });
+
 
